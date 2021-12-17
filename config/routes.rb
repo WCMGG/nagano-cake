a0a932cb8984289d5b904cb2a268bac824806a7f
vitamin332 <gtztmy3@gmail.com>
2021-12-17 05:19:43 +0000

    Add view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  root "public/homes#top"
+  
+  
+  
   # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords,], controllers: {
@@ -12,4 +17,5 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
