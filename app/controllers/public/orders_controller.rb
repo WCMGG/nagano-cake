67639542b93d603eaf1a621019307eafd9a931ab
mmhs2 <m.m.hs260056@ezweb.ne.jp>
2021-12-21 07:35:08 +0000

    add order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,20 +1,46 @@
 class Public::OrdersController < ApplicationController
   def new
+    @new_order = Order.new
+    @order = current_customer
   end
-  
+
   def index
   end
-  
+
   def show
   end
-  
+
   def complete
+    # if select_method = 0 
+    #   @order.postal_code = current_customer.postal_code
+    #   @order.address = current_customer.address
+    #   @order.name = current_customer.name
+    # else select_method = 1
+    #   @address=address.find(palams[:address_id])
+    #   @order.postal_code = @address.postal_code
+    #   @order.address = @address.address
+    #   @order.name = @address.name
+    # else select_method = 2
+      
+    #   @order.postal_code = @.postal_code
+    #   @order.address = @.address
+    #   @order.name = @.name
+    # end
   end
-  
+
   def create
   end
-  
+
   def confirm
+  end
+  
+  
+  
+  private
+  
+  
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
   
 end
