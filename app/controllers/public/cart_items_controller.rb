dbc1e5c52ae0b6aad8920ec58b73f3766dc7491a
augustleague <shanyuanshengye896@gmail.com>
2021-12-23 02:07:56 +0000

    Add Customer/item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,17 +1,28 @@
 class Public::CartItemsController < ApplicationController
   def index
+
   end
-  
+
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+      redirect_to cart_items_path
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   def destroy_all
   end
-  
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
+  end
+
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,8 +1,12 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.where(is_active: true).page(params[:page]).per(8)
+    @quantity = Item.count
   end
-  
+
   def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
 
 end
