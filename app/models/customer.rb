802520f5809e09529089200a37197c431eef9628
vitamin332 <gtztmy3@gmail.com>
2021-12-23 06:10:24 +0000

    fix cart_items

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,15 +1,16 @@
 class Customer < ApplicationRecord
-  
+
   has_many :addresses
-  
+  has_many :cart_items
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   # 退会機能
   def active_for_authentication?
     super && (self.is_deleted == false)
   end
-  
+
 end
