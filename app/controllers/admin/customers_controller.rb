e389851465165932963638518a3fcdf77aef9676
mmhs2 <m.m.hs260056@ezweb.ne.jp>
2021-12-24 06:30:33 +0000

    Add admin/order/show/index

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CustomersController < ApplicationController
 
   def index
-    @customers = Customer.page(params[:page])
+    @customers = Customer.page(params[:page]).per(10)
   end
 
   def show
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,27 @@
-class Admin::OrdersController < ApplicationController
-  def show
-  end 
-  
-  def index
-  end 
-  
-  def update
-  end 
-  
-end
+  class Admin::OrdersController < ApplicationController
+   def index
+    @orders = Order.all
+    @order = Order.find(params[:id])
+    @orders_page = Customer.page(params[:page]).per(10)
+   end
+   
+   def show
+     @customer = Customer.find(params[:id])
+     @order = Order.find(params[:id])
+     @item = item.find(params[:id])
+     @cart_item = Cart_item.find(params[:id])
+     @ordered_details = @order.ordered_details
+   end
+   
+   def subtotal
+    item.with_tax_price * amount
+   end
+   
+   def update
+    order = Order.find(params[:id])
+    status = params[:order][:status].to_i
+    order.update(status: status)
+    redirect_to admin_order_path(order)
+   end  
+    
+  end
