b9c9b3f00d70c9b9469f771e9438147d6b2dbd66
vitamin332 <gtztmy3@gmail.com>
2021-12-24 06:23:57 +0000

    Fix cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,18 +7,27 @@ class Public::CartItemsController < ApplicationController
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
     @cart_item.save
-      redirect_to cart_items_path
-      
-    # if current_customer.cart_items.find_by(item_id: [])
-    # end
-    
+      redirect_to cart_items_path and return
+
+    if cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+       cart_item.amount += params[:cart_item][:amount].to_i
+       cart_item.save
+       redirect_to cart_items_path and return
+
+    elsif @cart_item.save
+       redirect_to cart_items_path and return
+    else
+       render 'index'
+
+    end
+
     # if Item.find_by(name: "ショートケーキ")
     # puts "商品が存在する"
     # else
     # puts "商品が存在しない"
     # end
-    
-    
+
+
   end
 
   # 小計として追加したメソッド
