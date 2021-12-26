fe666efc6167f17d97a4dd6b21b2a76ec62e679b
vitamin332 <gtztmy3@gmail.com>
2021-12-26 07:37:50 +0000

    finish views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   # flashメッセージ（Bootstrapで使えるように）
   add_flash_types :success, :info, :warning, :danger
-  
-  private
-  
-  def after_sign_in_path_for(resource_or_scope)
-    if resource_or_scope.is_a?(Customer)
-        customers_path
-    else
-        root_path
+  before_action :configure_sign_up_params, only: [:create], if: :devise_controller?
+
+  def after_sign_up_path_for(resource)
+    case resource
+    when Admin
+      admin_customers_path
+    when Customer
+      customers_path
     end
   end
 
@@ -20,6 +20,12 @@ class ApplicationController < ActionController::Base
     else
         root_path
     end
+  end
+
+protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :postal_code, :phone_number])
   end
 
 end
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -38,12 +37,8 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  protected
-
   # If you have extra params to permit, append them to the sanitizer.
-   def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :postal_code, :phone_number])
-   end
+
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
