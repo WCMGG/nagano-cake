8b80b557ce9d57f1194d82dd04bcbe87db4d69a7
vitamin332 <gtztmy3@gmail.com>
2021-12-26 08:06:42 +0000

    finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   # flashメッセージ（Bootstrapで使えるように）
   add_flash_types :success, :info, :warning, :danger
-  before_action :configure_sign_up_params, only: [:create], if: :devise_controller?
-
+  # before_action :configure_sign_up_params, only: [:create], if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_up_path_for(resource)
     case resource
     when Admin
@@ -22,9 +22,20 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_customers_path
+    when Customer
+      customers_path
+    end
+  end
+
+
+
 protected
 
-  def configure_sign_up_params
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :postal_code, :phone_number])
   end
 
